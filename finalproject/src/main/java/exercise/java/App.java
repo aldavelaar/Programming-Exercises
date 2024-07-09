288cec095fc66a8de129c7c31ea3cda36048dc6f
Developer Andrew <109871175+aldavelaar@users.noreply.github.com>
2024-07-09 14:05:24 -0400

    Add files via upload

--- finalproject/src/main/java/exercise/java/App.java
+++ finalproject/src/main/java/exercise/java/App.java
@@ -1,40 +1,48 @@
 package exercise.java;
 
-  import javafx.application.Application;
-  import javafx.scene.Scene;
-  import javafx.scene.control.Label;
-  import javafx.scene.layout.BorderPane;
-  import javafx.scene.layout.GridPane;
-  import javafx.scene.layout.Pane;
-  import javafx.scene.paint.Color;
-  import javafx.scene.shape.Ellipse;
-  import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 
   public class App extends Application {
     // Indicate which player has a turn, initially it is the X player
-    private char whoseTurn = 'X';
+    private char whoseTurn = 'Y';
 
     // Create and initialize cell
     private final Cell[][] cell = new Cell[6][7];
 
     // Create and initialize a status label
-    private final Label lblStatus = new Label("X's turn to play");
+    private final static Label lblStatus = new Label("Yellow's turn to play");
 
     @SuppressWarnings("exports")
     @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
+      lblStatus.setFont(Font.font("Verdana", FontWeight.BOLD, FontPosture.REGULAR, 30));
+      lblStatus.setTextFill(Color.YELLOW);
+
       // Pane to hold cell
       GridPane pane = new GridPane();
+      pane.setStyle("-fx-border-color: black;");
       for (int i = 0; i < 6; i++)
         for (int j = 0; j < 7; j++)
           pane.add(cell[i][j] = new Cell(), j, i);
 
       BorderPane borderPane = new BorderPane();
+      borderPane.setStyle("-fx-background-color: lightblue;");
+      borderPane.setTop(lblStatus);
       borderPane.setCenter(pane);
-      borderPane.setBottom(lblStatus);
 
       // Create a scene and place it in the stage
-      Scene scene = new Scene(borderPane, 700, 600);
+      Scene scene = new Scene(borderPane, 700, 600, Color.YELLOW);
       primaryStage.setTitle("Connect 4"); // Set the stage title
       primaryStage.setScene(scene); // Place the scene in the stage
       primaryStage.show(); // Display the stage
@@ -50,32 +58,53 @@ package exercise.java;
       return true;
     }
 
+    public boolean isLegal() {
+      for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 7; j++)
+          if (cell[i][j].getToken() != ' ' && cell[i + 1][j].getToken() == ' ')
+            return false;
+
+      return true;
+    }
+
     /** Determine if the player with the specified token wins */
     public boolean isWon(char token) {
       for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 4; j++) {
+          if (cell[i][j].getToken() == token
+              && cell[i][j + 1].getToken() == token
+              && cell[i][j + 2].getToken() == token
+              && cell[i][j + 3].getToken() == token) {
+          return true;
+          }
+        }
+      }
+      for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 7; j++) {
           if (cell[i][j].getToken() == token
-              && cell[i][j + 1].getToken() == token
-              && cell[i][j + 2].getToken() == token
-              && cell[i][j + 3].getToken() == token) {
+              && cell[i + 1][j].getToken() == token
+              && cell[i + 2][j].getToken() == token
+              && cell[i + 3][j].getToken() == token) {
           return true;
           }
-          if (cell[i][j].getToken() == token
-              && cell[i][j + 1].getToken() == token
-              && cell[i][j + 2].getToken() == token
-              && cell[i][j + 3].getToken() == token) {
-          return true;
-          }
+        }
+      }
+      for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
           if (cell[i][j].getToken() == token
               && cell[i + 1][j + 1].getToken() == token
               && cell[i + 2][j + 2].getToken() == token
               && cell[i + 3][j + 3].getToken() == token) {
           return true;
           }
+        }
+      }
+      for (int i = 0; i < 3; i++) {
+        for (int j = 3; j < 7; j++) {
           if (cell[i][j].getToken() == token
-              && cell[i - 1][j + 1].getToken() == token
-              && cell[i - 2][j + 2].getToken() == token
-              && cell[i - 3][j + 3].getToken() == token) {
+              && cell[i + 1][j - 1].getToken() == token
+              && cell[i + 2][j - 2].getToken() == token
+              && cell[i + 3][j - 3].getToken() == token) {
           return true;
           }
         }
@@ -101,8 +130,6 @@ package exercise.java;
 
       /** Set a new token */
       public void setToken(char c) {
-        token = c;
-        
         Ellipse ellipse = new Ellipse(this.getWidth() / 2,
         this.getHeight() / 2, this.getWidth() / 2 - 10,
         this.getHeight() / 2 - 10);
@@ -115,14 +142,13 @@ package exercise.java;
         ellipse.radiusYProperty().bind(
         this.heightProperty().divide(2).subtract(10));
 
-        if (token == 'X') {
-          ellipse.setStroke(Color.YELLOWGREEN);
+        if (token == 'Y') {
           ellipse.setFill(Color.YELLOW);
         }
-        else if (token == 'O') {
-          ellipse.setStroke(Color.DARKRED);
+        else if (token == 'R') {
           ellipse.setFill(Color.RED);
         }
+        ellipse.setStroke(Color.BLACK);
 
         getChildren().add(ellipse); // Add the ellipse to the pane
 
@@ -132,24 +158,35 @@ package exercise.java;
       private void handleMouseClick() {
         // If cell is empty and game is not over
         if (token == ' ' && whoseTurn != ' ') {
-          setToken(whoseTurn); // Set token in the cell
-
-          // Check game status
-          if (isWon(whoseTurn)) {
-            lblStatus.setText(whoseTurn + " won! The game is over");
-            whoseTurn = ' '; // Game is over
-          }
-          else if (isFull()) {
-            lblStatus.setText("Draw! The game is over");
-            whoseTurn = ' '; // Game is over
-          }
-          else if (whoseTurn == 'X') {
-            whoseTurn = 'O';
-            lblStatus.setText(whoseTurn + "'s turn");
-          }
-          else if (whoseTurn == 'O') {
-            whoseTurn = 'X';
-            lblStatus.setText(whoseTurn + "'s turn");
+          token = whoseTurn; // Set token in the cell
+          if (isLegal()) {
+            setToken(whoseTurn);
+            // Check game status
+            if (isWon(whoseTurn)) {
+              if (whoseTurn == 'Y') {
+                lblStatus.setText("Yellow won! The game is over");
+              }
+              if (whoseTurn == 'R') {
+                lblStatus.setText("Red won! The game is over");
+              }
+              whoseTurn = ' '; // Game is over
+            }
+            else if (isFull()) {
+              lblStatus.setText("Draw! The game is over");
+              whoseTurn = ' '; // Game is over
+            }
+            else if (whoseTurn == 'Y') {
+              whoseTurn = 'R';
+              lblStatus.setText("Red's turn");
+              lblStatus.setTextFill(Color.RED);
+            }
+            else if (whoseTurn == 'R') {
+              whoseTurn = 'Y';
+              lblStatus.setText("Yellow's turn");
+              lblStatus.setTextFill(Color.YELLOW);
+            }
+          } else {
+            token = ' ';
           }
         }
       }
--- finalproject/src/main/java/module-info.java
+++ finalproject/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module exercise.java {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens exercise.java to javafx.fxml;
     exports exercise.java;
